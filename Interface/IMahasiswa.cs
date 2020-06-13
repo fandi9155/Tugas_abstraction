41c705b66631cb36d773195b832970998fa4e700
fandi9155 <61773118+fandi9155@users.noreply.github.com>
2020-06-13 22:03:55 +0700

    Add files via upload

---
+++ Interface/IMahasiswa.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tugas_Lab_7.Interface
+{
+    interface IMahasiswa
+    {
+        void Biodata();
+    }
+}
---
+++ Interface/KTP.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tugas_Lab_7.Interface
+{
+    class KTP : IMahasiswa
+    {
+        public void Biodata()
+        {
+            {
+                Console.WriteLine();
+                Console.WriteLine("    KTP ( Kartu Tanda Penduduk ) ");
+                Console.WriteLine("Nama     : Mukhamad yusuf afandi");
+                Console.WriteLine("Alamat   : Rembang");
+                Console.WriteLine("Telp  : 08134614622 ");
+            }
+        }
+    }
+        
+   
+}
